f5d40cbdadbef3ee4ddf9702d309075816c82d72
kedani <75819419+kedani@users.noreply.github.com>
2020-12-29 11:40:09 +0900

    Issue1~3の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,12 +34,8 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = # wdayメソッドを用いて取得した数値
-      if wday_num > 7
-        wday_num = wday_num -7
-      end
 
-      days = { :month => (@todays_date + x).month, :date => @todays_date.day + x, :plans => today_plans, wday => [0]}
+      days = { :month => (@todays_date + x).month, :date => @todays_date.day + x, :plans => today_plans}
       @week_days.push(days)
     end
 
